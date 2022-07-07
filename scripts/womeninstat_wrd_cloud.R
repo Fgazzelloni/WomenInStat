dd911a5a227ac1ef11680ef46a3a6dbd9d605b05
Federica Gazzelloni <docksbox@pm.me>
2022-07-07 11:59:44 +0200

    wordclou script

---
+++ scripts/womeninstat_wrd_cloud.R
@@ -0,0 +1,41 @@
+library(rtweet)        # Used for extracting the tweets
+library(tm)            # Text mining cleaning
+library(stringr)       # Removing characters
+library(qdapRegex)     # Removing URLs 
+library(wordcloud2)    # Creating the wordcloud
+
+
+tweets<- get_timelines(c("WomenInStat"), n = 200) #WomenInStat
+text <- str_c(tweets$text, collapse = "")
+text <- 
+  text %>%
+  str_remove("\\n") %>%                   # remove linebreaks
+  rm_twitter_url() %>%                    # Remove URLS
+  rm_url() %>%
+  str_remove_all("#\\S+") %>%             # Remove any hashtags
+  str_remove_all("@\\S+") %>%             # Remove any @ mentions
+  removeWords(stopwords("english")) %>%   # Remove common words (a, the, it etc.)
+  removeNumbers() %>%
+  stripWhitespace() %>%
+  removeWords(c("amp"))
+
+
+textCorpus <- 
+  Corpus(VectorSource(text)) %>%
+  TermDocumentMatrix() %>%
+  as.matrix()
+
+textCorpus <- sort(rowSums(textCorpus), decreasing=TRUE)
+textCorpus <- data.frame(word = names(textCorpus), 
+                         freq=textCorpus, 
+                         row.names = NULL)
+
+
+# build wordcloud 
+wordcloud <- wordcloud2(data = textCorpus, 
+                        minRotation = 0, 
+                        maxRotation = 0, 
+                        ellipticity = 0.6)
+wordcloud
+
+
